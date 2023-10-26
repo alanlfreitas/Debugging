1c4e425b85cfd59c44b57a5236646ee829e13966
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-10-26 16:28:56 +0000

    git commit -m "feat(bug 2): solved bug 2 by changing parameter in RANDINT."

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 ############DEBUGGING#####################
 
-# Describe Problem
-def my_function():
-  for i in range(1, 21): #bug found here, cause it was counting from 1 to 19 originally, now it's counting til 20
-    if i == 20: 
-      print("You got it")
-my_function()
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 21): #bug found here, cause it was counting from 1 to 19 originally, now it's counting til 20
+#     if i == 20: 
+#       print("You got it")
+# my_function()
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5) #Bug here, trying to locate a number in an inexistant position(6) 
+print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
